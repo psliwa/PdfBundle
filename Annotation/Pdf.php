1b8aa65ee069e32524b1d84a261cad900931d8a5
psliwa <peter.pl7@gmail.com>
2011-10-09 19:23:19 +0200

    annotation fix

--- Annotation/Pdf.php
+++ Annotation/Pdf.php
@@ -8,9 +8,12 @@
 
 namespace Ps\PdfBundle\Annotation;
 
+use Doctrine\Common\Annotations\Annotation;
+
 /**
  * Pdf annotation
  * 
+ * @Annotation
  * @author Piotr Śliwa <peter.pl7@gmail.com>
  */
 class Pdf
