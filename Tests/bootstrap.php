36b2f75f0866afab9a03ae4be9162ad3be792694
psliwa <peter.pl7@gmail.com>
2012-01-31 20:51:58 +0100

    fix

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,16 +1,12 @@
 <?php
 
-//$vendorDir = __DIR__.'/../vendor';
-$vendorDir = __DIR__.'/../../../../vendor';
+$vendorDir = __DIR__.'/../vendor';
 require_once $vendorDir.'/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 
-use Symfony\Component\ClassLoader\UniversalClassLoader;
-
-
-$loader = new UniversalClassLoader();
+$loader = new Symfony\Component\ClassLoader\UniversalClassLoader();
 $loader->registerNamespaces(array(
     'Symfony' => $vendorDir.'/symfony/src',
-    'Doctrine\\Common' => $vendorDir.'/DoctrineCommon/lib',
+    'Doctrine\\Common' => $vendorDir.'/Doctrine-Common/lib',
     'PHPPdf' => $vendorDir.'/PHPPdf/lib',
     'Imagine' => $vendorDir.'/Imagine/lib',
     'Zend' => $vendorDir.'/Zend/library'
@@ -19,7 +15,7 @@ $loader->registerNamespaces(array(
 spl_autoload_register(function($class){
     if(strpos($class, 'Ps\PdfBundle') === 0)
     {
-        return require_once __DIR_.'/../'.str_replace('\\', '/', substr($class, 13)).'.php';
+        return require_once __DIR__.'/../'.str_replace('\\', '/', substr($class, 13)).'.php';
     }
 });
 
