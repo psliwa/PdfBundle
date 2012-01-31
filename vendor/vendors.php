495171f63499d408e9109c5a42f03ad010ca0e12
psliwa <peter.pl7@gmail.com>
2012-01-31 21:04:47 +0100

    fix ;(

--- vendor/vendors.php
+++ vendor/vendors.php
@@ -7,7 +7,8 @@ $deps = array(
     array('Doctrine\Common', 'git://github.com/doctrine/common.git', isset($_ENV['DOCTRINE_COMMON_VERSION']) ? $_ENV['DOCTRINE_COMMON_VERSION'] : '2.1.4'),
     array('Markdown', 'git://github.com/wolfie/php-markdown.git', 'd464071334'),
     array('Zend', 'git://github.com/zendframework/zf2.git', 'master'),
-    array('Imagine', 'git://github.com/avalanche123/Imagine.git', 'v0.2.6')
+    array('Imagine', 'git://github.com/avalanche123/Imagine.git', 'v0.2.6'),
+    array('PHPPdf', 'git@github.com:psliwa/PHPPdf.git', 'origin/1.1.x'),
 );
 
 foreach ($deps as $dep) {
